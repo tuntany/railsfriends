f0367048a6f3fac29409a0eaf0b00bd00056cd6a
tany <tany@uni-bremen.de>
2023-01-31 19:22:29 +0100

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  
+  def index
+
+
+  end
+
+  def about
+    @about_me = "My name is Tuntany and welcome to my website! Write me to tany@uni-bremen.de!"
+  end
+
+end
